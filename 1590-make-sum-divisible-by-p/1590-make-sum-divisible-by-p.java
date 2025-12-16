e1be876db2da8582289b8fe375dc371adb4e077a
AMEEN BASITH_K <231001012@rajalakshmi.edu.in>
2025-12-16 16:43:53 +0530

    Time: 26 ms (94.11%), Space: 99.3 MB (47.73%) - LeetHub

---
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minSubarray(int[] nums, int p) {
+        long sum = 0;
+        for (int x : nums) sum += x;
+        int rem = (int)(sum % p);
+        if (rem == 0) return 0;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+
+        long prefix = 0;
+        int ans = nums.length;
+
+        for (int i = 0; i < nums.length; i++) {
+            prefix = (prefix + nums[i]) % p;
+            int need = (int)((prefix - rem + p) % p);
+
+            if (map.containsKey(need)) {
+                ans = Math.min(ans, i - map.get(need));
+            }
+            map.put((int)prefix, i);
+        }
+
+        return ans == nums.length ? -1 : ans;
+    }
+}
